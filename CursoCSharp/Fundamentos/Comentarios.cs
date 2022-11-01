0bf4ca922ac1349f262647a57e4df9fc348c4421
Jeová Ramos <jeova.ramos@gabriel.com.br>
2022-10-31 22:33:09 -0300

    Comentários.

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CursoCSharp.Fundamentos
+{
+    public class Comentarios
+    {
+        /// <summary>
+        /// Comentários XML.
+        /// </summary>
+        public static void Executar()
+        {
+            // Comentário simples.
+            Console.WriteLine("Código limpo é sempre melhor.");
+
+            /*
+             * Comentátios de
+             * múltiplas linhas
+             */
+
+            Console.WriteLine("C# possui XML comments.");
+        }
+    }
+}
+
