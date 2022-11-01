ffab96d64277a3e2dd955069bd82e73efec9d6e2
Jeová Ramos <jeova.ramos@gabriel.com.br>
2022-10-31 22:10:08 -0300

    First commit.

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CursoCSharp.Fundamentos
+{
+    public class PrimeiroPrograma
+    {
+        public static void Executar()
+        {
+            Console.Write("Primeiro ");
+            Console.WriteLine("programa!");
+            Console.Write("Terminou.");
+        }
+    }
+}
+
