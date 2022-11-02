60d02ebff12b1c7dd41c9043803f798c144ee6b6
jeova.ramos <jeova.ramos@gabriel.com.br>
2022-11-02 18:54:09 -0300

    Inferência de tipos.

---
+++ CursoCSharp/Fundamentos/Inferencia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CursoCSharp.Fundamentos
+{
+    public class Inferencia
+    {
+        public static void Executar()
+        {
+            //Variáveis implícitas devem ser inicializadas
+            var nome = "Jeová";
+            nome = "J";
+            //nome = 1;
+            Console.WriteLine(nome);
+
+            int idade;
+            idade = 25;
+            Console.WriteLine(idade);
+
+        }
+    }
+}
+
