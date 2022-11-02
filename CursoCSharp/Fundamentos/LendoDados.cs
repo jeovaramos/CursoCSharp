bb576f60bcfe7b8e9d77ea1b186f6f699ca110b4
jeova.ramos <jeova_j@hotmail.com>
2022-11-02 19:49:02 -0300

    Lendo dados do terminal.

---
+++ CursoCSharp/Fundamentos/LendoDados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+namespace CursoCSharp.Fundamentos
+{
+    public class LendoDados
+    {
+        public static void Executar()
+        {
+            Console.Write("Qual é o seu nome? ");
+            string? nome = Console.ReadLine();
+
+            Console.Write("Qual é a sua idade? ");
+            int? idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Qual o seu Salário? ");
+            double? salario = double.Parse(Console.ReadLine(),
+                CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"{nome} tem {idade} anos e ganha R${salario}.");
+
+        }
+    }
+}
+
