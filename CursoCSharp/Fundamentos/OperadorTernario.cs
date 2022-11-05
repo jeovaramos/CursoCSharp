cc6bced02c1e01c6e1fda5d27861225a2e95e0f8
jeovaramos <silvajunior.jeova@gmail.com>
2022-11-05 18:33:47 -0300

    Operador ternário.

---
+++ CursoCSharp/Fundamentos/OperadorTernario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CursoCSharp.Fundamentos
+{
+    public class OperadorTernario
+    {
+        public static void Executar()
+        {
+            var nota = 7.0;
+            string resultado = nota >= 7.0 ? "Aprovado" : "Reprovado";
+            Console.WriteLine(resultado);
+
+            bool bomComportamento = false;
+            string resultado2 = nota >= 7.0 && bomComportamento
+                ? "Aprovado" : "Reprovado";
+            Console.WriteLine(resultado2);
+        }
+    }
+}
+
