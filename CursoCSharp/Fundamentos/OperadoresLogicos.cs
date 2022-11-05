57000d3a1b51a9c5984288c4ae8c4ae8c9fc1395
jeovaramos <silvajunior.jeova@gmail.com>
2022-11-05 18:03:45 -0300

    Operadores lógicos.

---
+++ CursoCSharp/Fundamentos/OperadoresLogicos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CursoCSharp.Fundamentos
+{
+    public class OperadoresLogicos
+    {
+        public static void Executar()
+        {
+            var executouTrabalho1 = true;
+            var executouTrabalho2 = false;
+
+            bool comprouTv50 = executouTrabalho1 && executouTrabalho2;
+            Console.WriteLine("Comprou a TV de 50''? {0}", comprouTv50);
+
+            var comprouSorvete = executouTrabalho1 || executouTrabalho2;
+            Console.WriteLine("Comprou o Sorvete? {0}", comprouSorvete);
+
+            bool comprouTv32 = executouTrabalho1 ^ executouTrabalho2;
+            Console.WriteLine("Comprou a TV de 32''? {0}", comprouTv32);
+
+            Console.WriteLine("Ficou na dieta? {0}", !comprouSorvete);
+
+        }
+    }
+}
+
