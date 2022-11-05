b356e7df4478322b0d7361a8deccad2466c158b5
jeovaramos <silvajunior.jeova@gmail.com>
2022-11-05 18:52:24 -0300

    Estrutura If

---
+++ CursoCSharp/EstruturasDeControle/EstruturaIf.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace CursoCSharp.EstruturasDeControle
+{
+    public class EstruturaIf
+    {
+        public static void Executar()
+        {
+            bool bomComportamento = false;
+            Console.Write("Digite a nota do aluno: ");
+            Double.TryParse(Console.ReadLine(), out double nota);
+
+            string entrada;
+            Console.Write("Aluno possui bom comportamento (S/N): ");
+            entrada = Console.ReadLine();
+
+            //if (entrada == "S" || entrada == "s")
+            //    bomComportamento = true;
+            //bomComportamento = entrada == "S" || entrada == "s";
+            bomComportamento = entrada.ToLower() == "s";
+
+            if (nota >= 9.0 && bomComportamento)
+            {
+                Console.WriteLine("Quadro de honra!");
+            }
+        }
+    }
+}
+
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 
 using CursoCSharp.Fundamentos;
+using CursoCSharp.EstruturasDeControle;
 
 namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                // Fundamentos
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentários - Fundamentos", Comentarios.Executar},
                 {"Variáveis e constantes - Fundamentos", VariaveisEConstantes.Executar},
@@ -22,6 +24,9 @@ namespace CursoCSharp {
                 {"Operadores de Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
                 {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar},
                 {"Operador Ternário - Fundamentos", OperadorTernario.Executar},
+
+                // Estruturas de controle
+                {"Estrutura If - Estruturas de Controle", EstruturaIf.Executar},
             });
 
             central.SelecionarEExecutar();
