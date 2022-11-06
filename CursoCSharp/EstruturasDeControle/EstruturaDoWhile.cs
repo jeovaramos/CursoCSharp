8ce371a69f68ab9c2c53ed79aaf9dfc966b0e9fa
jeovaramos <silvajunior.jeova@gmail.com>
2022-11-06 11:43:03 -0300

    Estrutura do while.

---
+++ CursoCSharp/EstruturasDeControle/EstruturaDoWhile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CursoCSharp.EstruturasDeControle
+{
+    public class EstruturaDoWhile
+    {
+        public static void Executar()
+        {
+            string entrada;
+
+            do
+            {
+                Console.WriteLine("Qual o seu nome?");
+                entrada = Console.ReadLine();
+
+                Console.WriteLine("Seja bem-vindo {0}", entrada);
+                Console.WriteLine("Deseja continuar? (s/N)");
+                entrada = Console.ReadLine();
+            } while (entrada.ToLower() == "s");
+            Console.WriteLine("Fim do programa.");
+        }
+    }
+}
+
