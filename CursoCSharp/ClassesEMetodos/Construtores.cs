f86c4eccdc51c770a421f1e4e78a28c13e3bc1c4
jeovaramos <silvajunior.jeova@gmail.com>
2022-11-08 15:53:47 -0300

    Construtores.

---
+++ CursoCSharp/ClassesEMetodos/Construtores.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace CursoCSharp.ClassesEMetodos
+{
+    class Carro
+    {
+        public string Modelo;
+        public string Fabricante;
+        public int Ano;
+
+        public Carro(string modelo, string fabricante, int ano)
+        {
+            Modelo = modelo; // Não é o padrão
+            Fabricante = fabricante;
+            Ano = ano;
+        }
+
+        public Carro() // Construtor vazio para manter o construtor padrão.
+        {
+
+        }
+    }
+
+    public class Construtores
+    {
+        public static void Executar()
+        {
+            var carro1 = new Carro(); // Construtor padrão
+            carro1.Fabricante = "BMW";
+            carro1.Modelo = "325i";
+            carro1.Ano = 2017;
+            Console.WriteLine(
+                $"{carro1.Fabricante} {carro1.Modelo} {carro1.Ano}");
+
+            var carro2 = new Carro("Ka", "Ford", 2018);
+            Console.WriteLine(
+                $"{carro2.Fabricante} {carro2.Modelo} {carro2.Ano}");
+
+            Carro carro3 = new Carro()
+            {
+                Fabricante = "Fiat",
+                Modelo = "Uno",
+                Ano = 2010
+            };
+            Console.WriteLine(
+                $"{carro3.Fabricante} {carro3.Modelo} {carro3.Ano}");
+        }
+    }
+}
+
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -40,6 +40,7 @@ namespace CursoCSharp {
 
                 // Classes e Métodos
                 {"Membros - Classes e Métodos", Membros.Executar},
+                {"Construtores - Classes e Métodos", Construtores.Executar},
 
             });
 
