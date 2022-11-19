18478885c152b5a845ba4e84111ef4b27b796219
jeovaramos <silvajunior.jeova@gmail.com>
2022-11-19 19:47:13 -0300

    Struct

---
+++ CursoCSharp/ClassesEMetodos/Struct.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace CursoCSharp.ClassesEMetodos
+{
+    interface Ponto
+    {
+        void MoverNaDiagonal(int delta);
+    }
+
+    struct Coordenada: Ponto
+    {
+        public int X;
+        public int Y;
+
+        public Coordenada(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void MoverNaDiagonal(int delta)
+        {
+            X += delta;
+            Y += delta;
+        }
+
+        public void PrintCoordenada()
+        {
+            Console.WriteLine("X = {0}, Y = {1}", X, Y);
+        }
+    }
+    public class Struct
+    {
+        public static void Executar()
+        {
+            Coordenada coordenadaInicial;
+            coordenadaInicial.X = 0;
+            coordenadaInicial.Y = 0;
+
+            coordenadaInicial.PrintCoordenada();
+            for (int ii = 1; ii <= 10; ii++) {
+                coordenadaInicial.MoverNaDiagonal(1);
+                coordenadaInicial.PrintCoordenada();
+
+            }
+
+        }
+    }
+}
+
