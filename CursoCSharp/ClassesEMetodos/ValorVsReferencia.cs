fbfb38ffe2795f8bfa60974fd227515d0ec51fae
jeovaramos <silvajunior.jeova@gmail.com>
2022-11-20 17:52:52 -0300

    Valor vs referência.

---
+++ CursoCSharp/ClassesEMetodos/ValorVsReferencia.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace CursoCSharp.ClassesEMetodos
+{
+    public class Dependente
+    {
+        public string Nome;
+        public int Idade;
+    }
+
+    class ValorVsReferencia
+    {
+        public static void Executar()
+        {
+            int numero = 3;  // int é uma struct
+            int copiaNumero = numero;
+            Console.WriteLine($"{numero} {copiaNumero}");
+
+            numero++;
+            Console.WriteLine($"{numero} {copiaNumero}");
+
+            Dependente dep = new Dependente
+            {
+                Nome = "Roberto",
+                Idade = 33
+            };
+
+            Dependente copiaDep = dep;
+
+            Console.WriteLine($"{dep.Nome} {copiaDep.Nome}");
+            Console.WriteLine($"{dep.Idade} {copiaDep.Idade}");
+
+            copiaDep.Nome = "Renato";
+            dep.Idade = 35;
+
+            Console.WriteLine($"{dep.Nome} {copiaDep.Nome}");
+            Console.WriteLine($"{dep.Idade} {copiaDep.Idade}");
+        }
+    }
+}
